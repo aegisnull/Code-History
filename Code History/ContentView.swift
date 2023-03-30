7cf6fab1d555be7246491d10735e0c71538f2560
Luis Tellez <luis@luistellez.com>
2023-03-29 22:08:33 -0700

    feat: change icon and text

--- Code History/ContentView.swift
+++ Code History/ContentView.swift
@@ -8,12 +8,14 @@
 import SwiftUI
 
 struct ContentView: View {
+    var name: String = "Luis"
     var body: some View {
         VStack {
-            Image(systemName: "globe")
+            Image(systemName: "person")
                 .imageScale(.large)
                 .foregroundColor(.accentColor)
-            Text("Hello, world!")
+            Text("Hello \(name)!")
+                .multilineTextAlignment(.center)
         }
         .padding()
     }
