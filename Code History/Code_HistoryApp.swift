55cb60c5a1bcc0d71e55081ea9a1483b166c6200
Luis Tellez <luis@luistellez.com>
2023-03-29 21:19:45 -0700

    Initial Commit

---
+++ Code History/Code_HistoryApp.swift
@@ -0,0 +1,17 @@
+//
+//  Code_HistoryApp.swift
+//  Code History
+//
+//  Created by Luis Tellez on 3/29/23.
+//
+
+import SwiftUI
+
+@main
+struct Code_HistoryApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
