c1055c257cbf59564b23b7ee6bec30cf1efef1a1
Luis Tellez <luis@luistellez.com>
2023-03-30 20:21:02 -0700

    feat: replace initial default content with first question and initial buttons

--- Code History/ContentView.swift
+++ Code History/ContentView.swift
@@ -8,16 +8,68 @@
 import SwiftUI
 
 struct ContentView: View {
-    var name: String = "Luis"
+    let mainColor = Color(red: 20/255, green: 28/255, blue: 58/255)
+    let accentColor = Color(red: 48/255, green: 105/255, blue: 240/255)
     var body: some View {
-        VStack {
-            Image(systemName: "person")
-                .imageScale(.large)
-                .foregroundColor(.accentColor)
-            Text("Hello \(name)!")
-                .multilineTextAlignment(.center)
+        ZStack {
+            mainColor.ignoresSafeArea()
+            VStack {
+                Text("1 / 10")
+                    .font(.callout)
+                    .multilineTextAlignment(.leading)
+                    .padding()
+                Text("It is a fire type Pokemon that evolves from Charmeleon. Can you name this Pokemon?")
+                    .padding()
+                    .font(.title)
+                    .bold()
+                    .multilineTextAlignment(.leading)
+                Spacer()
+                HStack {
+                    Button(action: {
+                        print("Selected option 1")
+                    }, label: {
+                        Text("Pikachu")
+                            .font(.body)
+                            .bold()
+                            .multilineTextAlignment(.center)
+                            .padding()
+                            .border(accentColor, width: 4)
+                    })
+                    Button(action: {
+                        print("Selected option 2")
+                    }, label: {
+                        Text("Charmander")
+                            .font(.body)
+                            .bold()
+                            .multilineTextAlignment(.center)
+                            .padding()
+                            .border(accentColor, width: 4)
+                    })
+                    Button(action: {
+                        print("Selected option 3")
+                    }, label: {
+                        Text("Eevee")
+                            .font(.body)
+                            .bold()
+                            .multilineTextAlignment(.center)
+                            .padding()
+                            .border(accentColor, width: 4)
+                    })
+                    Button(action: {
+                        print("Selected option 4")
+                    }, label: {
+                        Text("Ekans")
+                            .font(.body)
+                            .bold()
+                            .multilineTextAlignment(.center)
+                            .padding()
+                            .border(accentColor, width: 4)
+                    })
+                    
+                }
+            }
         }
-        .padding()
+        .foregroundColor(.white)
     }
 }
 
